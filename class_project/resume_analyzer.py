a9cfb43bfbb4055a621b68d34f32446218ea4866
kalyanimurkute <murkutekalyani259@gmail.com>
2026-07-27 11:32:03 +0530

    add skill

--- class_project/resume_analyzer.py
+++ class_project/resume_analyzer.py
@@ -14,4 +14,15 @@ o	JavaScript
 •	Show missing skills 
 •	Suggest improvements
 '''
-resume=input("Enter the resume text: ")
+from colorama import Fore, Style, Back,init
+init()
+resume=input("Enter the resume text: ").lower()
+skills=["python","django","rest api","sql","mysql","git","html","css","javascript"]
+matched_skills=[]
+missing_skills=[]
+# check for each skill in the resume
+for skill in skills:
+    if skill in resume:
+        matched_skills.append(skill)
+    else:
+        missing_skills.append(skill)
