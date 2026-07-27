434845ae464fdc20b854320a9f09388fc03432a5
kalyanimurkute <murkutekalyani259@gmail.com>
2026-07-27 11:12:10 +0530

    add comment

---
+++ resume_analyzer.py
@@ -0,0 +1,16 @@
+'''
+•	Read resume text 
+•	Count keywords like: 
+o	Python 
+o	Django 
+o	REST API 
+o	SQL 
+o	MySQL 
+o	Git 
+o	HTML 
+o	CSS 
+o	JavaScript 
+•	Display match percentage 
+•	Show missing skills 
+•	Suggest improvements
+'''
